216e3b1566e70d42e36fc1c78d1f157e9bea121a
Fernando <fernando@flashbob.de>
2025-01-21 21:10:03 +0100

    Add codeium

--- init.lua
+++ init.lua
@@ -838,6 +838,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'codeium' },
         },
       }
     end,
